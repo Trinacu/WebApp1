76cb3e9f2e93a8658a8d6e0c6bf432604ed20a76
Trinacu <aleks.tratnik@yahoo.com>
2017-08-02 01:04:10 +0200

    first commit

---
+++ WebApplication1/Controllers/oglasiController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace WebApplication1.Controllers
+{
+    public class oglasiController : Controller
+    {
+        oglasiContext2 db = new oglasiContext2();
+
+        public ActionResult Index()
+        {
+            return View(db.oglas.ToList());
+        }
+        [Authorize]
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ogla oglasi)
+        {
+            if(ModelState.IsValid)
+            {
+                db.oglas.Add(oglasi);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(oglasi);
+        }
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            ogla oglasi = db.oglas.Find(id);
+            if (oglasi == null)
+            {
+                return HttpNotFound();
+            }
+            return View(oglasi);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include ="OglasID,start,cilj,cas,strosek,opis,st_oseb")] ogla oglasi)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(oglasi).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(oglasi);
+        }
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[oglas] (
+    [OglasID]      INT    IDENTITY (1, 1) NOT NULL,
+    [start]   NVARCHAR (50)  NULL,
+    [cilj]    NVARCHAR (50)  NULL,
+    [cas]	  TIME			 NULL,
+    [strosek] INT			NULL,
+    [opis]    NVARCHAR (200) NULL,
+    [st_oseb] INT         NULL,
+    PRIMARY KEY CLUSTERED ([OglasID] ASC), 
+);
+
